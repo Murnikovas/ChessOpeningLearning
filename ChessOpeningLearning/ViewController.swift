7477339357f4f9c3157be8e3cd0d5ebee15e407a
Pablo Murnikovas <pablo.murnikovas@thinkupsoft.com>
2021-03-04 23:52:28 -0300

    Initial Commit

---
+++ ChessOpeningLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChessOpeningLearning
+//
+//  Created by Pablo Murnikovas on 4/3/21.
+//  Copyright © 2021 Pablo Murnikovas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
